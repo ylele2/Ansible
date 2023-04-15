e4732f464159c6e1c97870e0006cce1c5d4d8d22
ylele2 <72615992+ylele2@users.noreply.github.com>
2023-04-15 15:43:22 +0530

    Add files via upload

--- ServerReport.py
+++ ServerReport.py
@@ -1,5 +1,6 @@
 import psutil
 import socket
+import matplotlib.pyplot as plt
 
 # Fetch server details using psutil and socket libraries
 hostname = socket.gethostname()
@@ -8,11 +9,12 @@ cpu_percent = psutil.cpu_percent(interval=1)
 memory_info = psutil.virtual_memory()
 disk_usage = psutil.disk_usage('/')
 
-# Generate HTML report
+# Generate HTML report with graphical representations
 html_report = f"""
 <html>
 <head>
     <title>Server Details</title>
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 </head>
 <body>
     <h1>Server Details</h1>
@@ -38,6 +40,46 @@ html_report = f"""
             <td>Total: {disk_usage.total} bytes, Used: {disk_usage.used} bytes, Free: {disk_usage.free} bytes</td>
         </tr>
     </table>
+    <h2>CPU Usage</h2>
+    <div id="cpu-usage"></div>
+    <h2>Memory Usage</h2>
+    <div id="memory-usage"></div>
+    <h2>Disk Usage</h2>
+    <div id="disk-usage"></div>
+    <script>
+        // Create CPU usage chart
+        Plotly.newPlot('cpu-usage', [{
+            x: ['CPU'],
+            y: [{cpu_percent}],
+            type: 'bar',
+            marker: {{
+                color: 'rgb(50, 171, 96)',
+                opacity: 0.7
+            }}
+        }]);
+
+        // Create memory usage chart
+        Plotly.newPlot('memory-usage', [{
+            x: ['Total', 'Available', 'Used'],
+            y: [{memory_info.total}, {memory_info.available}, {memory_info.used}],
+            type: 'bar',
+            marker: {{
+                color: 'rgb(128, 0, 128)',
+                opacity: 0.7
+            }}
+        }]);
+
+        // Create disk usage chart
+        Plotly.newPlot('disk-usage', [{
+            x: ['Total', 'Used', 'Free'],
+            y: [{disk_usage.total}, {disk_usage.used}, {disk_usage.free}],
+            type: 'bar',
+            marker: {{
+                color: 'rgb(255, 165, 0)',
+                opacity: 0.7
+            }}
+        }]);
+    </script>
 </body>
 </html>
 """
